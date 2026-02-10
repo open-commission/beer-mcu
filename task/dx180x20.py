6ab526a11ef8ce281fa4de5266f28cf1916be918
nebula <nebula@nebulacoding.fun>
2026-02-10 22:14:25 +0800

    uart

--- task/dx180x20.py
+++ task/dx180x20.py
@@ -1,8 +1,19 @@
 import uasyncio as asyncio
 import machine, onewire, ds18x20
+from data.singleton_data import get_data_manager
 
 
-async def dx_read_task(pin):
+async def dx_read_task(pin, device_id):
+    """
+    DS18B20温度读取任务
+    
+    Args:
+        pin: GPIO引脚号
+        device_id: 设备ID ('device1' 或 'device2')
+    """
+    # 获取数据管理器
+    data_mgr = get_data_manager()
+    
     ds_pin = machine.Pin(pin)
     ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 
@@ -13,13 +24,25 @@ async def dx_read_task(pin):
         ds_sensor.convert_temp()
         await asyncio.sleep_ms(750)
         for rom in roms:
-            print('Found DS devices: {} -> {}'.format(pin, ds_sensor.read_temp(rom)))
+            temp = ds_sensor.read_temp(rom)
+            print('DS{}温度: {:.2f}°C'.format(pin, temp))
+            
+            # 更新对应设备的温度数据
+            if device_id == 'device1':
+                data_mgr.device1_temp = temp
+                print('✅ 设备1温度已更新: {:.2f}°C'.format(temp))
+            elif device_id == 'device2':
+                data_mgr.device2_temp = temp
+                print('✅ 设备2温度已更新: {:.2f}°C'.format(temp))
+                
         await asyncio.sleep(2)
 
 
 async def dx_task_1():
-    await dx_read_task(9)
+    """设备1温度读取任务 (GPIO9)"""
+    await dx_read_task(9, 'device1')
 
 
 async def dx_task_2():
-    await dx_read_task(7)
+    """设备2温度读取任务 (GPIO7)"""
+    await dx_read_task(7, 'device2')
